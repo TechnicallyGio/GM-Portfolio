935684e888e4b0dcfb11e9b2c2fa253f566c00ac
Gio <gio@shortfinal.app>
2025-05-10 20:20:16 -0500

    Update Responsiveness

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,10 +1,26 @@
+import { Icon } from "@iconify/react";
+
 export function Footer() {
   return (
-    <footer className="flex flex-row justify-between">
-      <div className="flex">
-        &copy; {new Date().getFullYear()} AORUX. All rights reserved.
+    <footer className="flex flex-col items-center justify-between px-6 py-4 md:flex-row">
+      <div className="mb-2 flex justify-center md:mb-0">
+        &copy; {new Date().getFullYear()} Giovanni Medrano. All rights reserved.
       </div>
-      <div className="flex">Privacy Policy | Terms of Service</div>
+      <div className="flex justify-center gap-4">
+        <a
+          href="https://linkedin.com/in/gmedrano/"
+          className="flex items-center hover:text-red-500 dark:hover:text-red-400"
+        >
+          <Icon icon="mdi:linkedin" className="mr-1" /> LinkedIn
+        </a>
+        <span className="hidden md:inline">|</span>
+        <a
+          href="https://github.com/technicallygio"
+          className="flex items-center hover:text-red-500 dark:hover:text-red-400"
+        >
+          <Icon icon="mdi:github" className="mr-1" /> GitHub
+        </a>
+      </div>
     </footer>
   );
 }
